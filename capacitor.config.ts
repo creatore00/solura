4497c87a20295a23359cbee6aefefe4cfbd4498e
Yassir <yassir.nini27@gmail.com>
2025-09-24 23:26:04 +0100

    make it better

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,9 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.solura.app',
   appName: 'Solura',
   webDir: 'www',
-  server: {
-    url: "https://solura.uk",   // carica direttamente il tuo server Node
-  }
 };
 
 export default config;
