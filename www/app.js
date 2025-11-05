8e02abcd3aa6e7646ea8eaec8b37216416af3763
Yassir <yassir.nini27@gmail.com>
2025-11-05 22:25:15 +0000

    make it better

--- www/app.js
+++
@@ -1,8 +0,0 @@
-import { Capacitor } from '@capacitor/core';
-
-document.addEventListener('deviceready', () => {
-  if (!Capacitor.isNativePlatform()) {
-    // fallback for web
-    window.location.href = 'https://solura.uk';
-  }
-});
